bab2db61c1ac2a850df0b976b6e2d5a7b75320f5
Tarocch1 <huiben.fang@gmail.com>
2025-07-20 15:51:25 +0800

    feat: prettier

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({ entry: ['./src/index.ts'], dts: true })
+export default defineConfig({
+  entry: ['./src/index.ts', './src/prettier.ts'],
+  dts: true,
+  // exports: true,
+})
